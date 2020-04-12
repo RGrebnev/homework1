77554b4620234321067a141668031d5030bfe847
RomanGrebnev <grebnevr@yandex.ru>
2020-04-12 20:34:01 +0000

    fix tests

--- junit/src/test/java/SampleTest.java
+++ junit/src/test/java/SampleTest.java
@@ -1,12 +1,11 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
 
 public class SampleTest {
 
--- testng/src/test/java/SimpleTest.java
+++ testng/src/test/java/SimpleTest.java
@@ -1,21 +1,26 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class SimpleTest {
     protected static WebDriver driver;
+    private Logger logger = LogManager.getLogger(SimpleTest.class);
 
     @BeforeTest
     public void setUp() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
+        logger.info("Драйвер поднят");
     }
     @Test
     public void openPage() {
         driver.get("https://otus.ru/");
+        logger.info("Открыта страница отус");
     }
 
     @AfterTest
